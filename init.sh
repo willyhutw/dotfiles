0345123eb2d84d44a3dfea9546cc77563eb3c20d
willyhu.tw <willyhu.tw@gmail.com>
2024-04-06 23:51:58 +0800

    Refactor init script.

--- init.sh
+++ init.sh
@@ -2,227 +2,179 @@
 
 set -u
 
-function install_pkgs {
-	sudo apt update &&
-		sudo apt install -y \
+function essentials {
+	sudo apt update -qq \
+		&& sudo apt install -qqy \
 			vim \
 			git \
 			tmux \
 			curl \
 			python3-pip \
-			xsel \
 			acpi \
-			sysstat \
 			lm-sensors \
+			sysstat \
+			xsel \
 			ripgrep \
 			fd-find
 }
 
-function check_btop {
-	echo "checking btop ..."
-	which btop &>/dev/null
-	if [ $? != 0 ]; then
-		echo "installing btop ..."
-		tag="v1.3.2"
-		curl -sLO https://github.com/aristocratos/btop/releases/download/$tag/btop-i686-linux-musl.tbz
-		tar -xf btop-i686-linux-musl.tbz
-		cd btop
-		sudo ./install.sh
-		cd ..
-	fi
-	echo "btop is ready!"
+#TODO: Build from source.
+function install_alacritty {
+	sudo snap install --classic alacritty
 }
 
-function check_alacritty {
-	echo "checking alacritty ..."
-	which alacritty &>/dev/null
-	if [ $? != 0 ]; then
-		echo "installing alacritty ..."
-		sudo snap install --classic alacritty
-	fi
-	echo "alacritty is ready!"
+function config_alacritty {
+	prog="alacritty"
+	theme="gruvbox_dark"
+	cfgDir=$HOME/.config/$prog
+	mkdir -p $cfgDir/themes
+	cp ./$prog/$prog.toml $cfgDir/$prog.toml
+	cp ./$prog/themes/$theme.toml $cfgDir/themes/$theme.toml
 }
 
-function update_alacritty_conf {
-	echo "updating alacritty config ..."
-	destDir=$HOME/.config/alacritty
-	mkdir -p $destDir/themes
-	cp ./alacritty/alacritty.toml $destDir/alacritty.toml
-	cp ./alacritty/themes/gruvbox_dark.toml $destDir/themes/gruvbox_dark.toml
-	echo "alacritty config has been updated!"
+function install_btop {
+	tag="v1.3.2"
+	prog="btop"
+	fileName="btop-i686-linux-musl.tbz"
+	curl -sLO https://github.com/aristocratos/btop/releases/download/$tag/$fileName
+	tar -xf $fileName
+	cd $prog
+	sudo ./install.sh
+	cd ..
+	rm -rf $prog
+	rm $fileName
 }
 
-function check_tpm {
-	echo "checking tpm ..."
-	if [ ! -d $destDir/tpm ]; then
-		echo "installing tpm ..."
-		tag="3.1.0"
-		url="https://github.com/tmux-plugins/tpm/archive/refs/tags/v$tag.tar.gz"
-		destDir="$HOME/.tmux/plugins"
-		curl -sLO $url
-		tar -zxf v$tag.tar.gz -C $destDir
-		mv $destDir/tpm-$tag $destDir/tpm
-		rm v$tag.tar.gz
-	fi
-	echo "tpm is ready!"
+function install_go {
+	fileName="go1.22.2.linux-amd64.tar.gz"
+	destDir="/usr/local"
+	curl -sLO https://go.dev/dl/$fileName
+	sudo rm -rf $destDir/go && sudo tar -C $destDir -xzf $fileName
+	rm ./$fileName
 }
 
-function update_tmux_conf {
-	echo "updating tmux config ..."
-	destDir=$HOME/.config/tmux
-	mkdir -p $destDir
-	cp ./tmux/tmux.conf $destDir/tmux.conf
-	echo "tmux config has been updated!"
+function install_helm {
+	tag="v3.14.3"
+	fileName="helm-$tag-linux-amd64.tar.gz"
+	curl -sLO https://get.helm.sh/$fileName
+	tar -zxf $fileName
+	sudo mv ./linux-amd64/helm /usr/local/bin/
+	rm -rf ./linux-amd64
+	rm $fileName
 }
 
-function update_vimrc {
-	echo "updating vim config ..."
-	cp ./vim/vimrc $HOME/.vimrc
-	echo "vim config has been updated!"
+function install_k9s {
+	tag="v0.32.4"
+	fileName="k9s_Linux_amd64.tar.gz"
+	curl -sLO https://github.com/derailed/k9s/releases/download/$tag/$fileName
+	sudo tar -zxf $fileName -C /usr/local/bin
+	rm $fileName
 }
 
-function check_neovim {
-	echo "checking neovim ..."
+function install_kubectl {
+	tag="v1.28.7"
+	fileName="kubectl"
+	curl -sLO https://dl.k8s.io/release/$tag/bin/linux/amd64/$fileName
+	sudo install -o root -g root -m 0755 kubectl /usr/local/bin/$fileName
+	rm $fileName
+}
+
+function config_nerdfonts {
+	tag="v3.2.0"
+	fontDir="/usr/local/share/fonts"
+	fonts=("Hack")
+	for font in "${fonts[@]}"; do
+		if [ ! -f $fontDir/${font}NerdFont-Regular.ttf ]; then
+			echo "$font not found! installing ..."
+			curl -sLO https://github.com/ryanoasis/nerd-fonts/releases/download/$tag/$font.zip
+			sudo unzip -qo $font.zip -d $fontDir
+			rm $font.zip
+		fi
+	done
+	fc-cache -f
+}
+
+function install_nvim {
 	tag="v0.9.5"
-	filename="nvim-linux64"
-	which nvim &>/dev/null
-	if [ $? != 0 ]; then
-		echo "installing neovim ..."
-		curl -sLO https://github.com/neovim/neovim/releases/download/$tag/$filename.tar.gz
-		sudo tar -zxf $filename.tar.gz -C /opt
-		sudo ln -s /opt/$filename/bin/nvim /usr/local/bin/nvim
-		rm $filename.tar.gz
-	fi
-	echo "neovim is ready!"
+	fileName="nvim-linux64"
+	curl -sLO https://github.com/neovim/neovim/releases/download/$tag/$fileName.tar.gz
+	sudo tar -zxf $fileName.tar.gz -C /opt
+	sudo ln -s /opt/$fileName/bin/nvim /usr/local/bin/nvim
+	rm $fileName.tar.gz
 }
 
-function setup_lazyvim {
-	echo "installing lazyvim ..."
+function config_nvim {
 	destDir="$HOME/.config/nvim"
 	rm -rf $HOME/.local/share/nvim
 	rm -rf $HOME/.local/state/nvim
 	rm -rf $destDir
 	cp -rf ./nvim $destDir
-	echo "lazyvim is ready!"
 }
 
-function check_nvm {
-	echo "checking nvm & node ..."
-	which nvm &>/dev/null
-	if [ $? != 0 ]; then
-		echo "installing nvm & node ..."
-		tag="v0.39.7"
-		curl -sLO https://raw.githubusercontent.com/nvm-sh/nvm/$tag/install.sh
-		chmod +x install.sh
-		./install.sh
-		export NVM_DIR="$HOME/.config/nvm"
-		[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"                   # This loads nvm
-		[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" # This loads nvm bash_completion
-		nvm install --lts
-		rm ./install.sh
+function install_nvm {
+	tag="v0.39.7"
+	nvm="$HOME/.config/nvm/nvm.sh"
+	if [ ! -f $nvm ]; then
+		curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/$tag/install.sh | bash
 	fi
-	echo "nvm & node is ready!"
 }
 
-function check_go {
-	echo "checking go ..."
-	which go &>/dev/null
-	if [ $? != 0 ]; then
-		echo "installing go ..."
-		filename="go1.22.2.linux-amd64.tar.gz"
-		targetDir="/usr/local"
-		curl -sLO https://go.dev/dl/$filename
-		sudo rm -rf $targetDir/go && sudo tar -C $targetDir -xzf $filename
-		rm ./$filename
+function config_tmux {
+	cfgDir=$HOME/.config/tmux
+	mkdir -p $cfgDir
+	cp ./tmux/tmux.conf $cfgDir/tmux.conf
+}
+
+function config_tpm {
+	tag="3.1.0"
+	pluginDir="$HOME/.tmux/plugins"
+	if [ ! -d $pluginDir/tpm ]; then
+		curl -sLO https://github.com/tmux-plugins/tpm/archive/refs/tags/v$tag.tar.gz
+		tar -zxf v$tag.tar.gz -C $pluginDir
+		mv $pluginDir/tpm-$tag $pluginDir/tpm
+		rm v$tag.tar.gz
 	fi
-	echo "go is ready!"
 }
 
-function setup_python_venv {
-	echo "installing python venv ..."
-	sudo pip install virtualenv
-	virtualenv venv
-	echo "python venv is ready!"
+function config_vim {
+	cp ./vim/vimrc $HOME/.vimrc
 }
 
-function setup_nerdfonts {
-	echo "installing nerd fonts ..."
-	fonts=("FiraMono" "Hack" "UbuntuMono")
-	for font in ${fonts[@]}; do
-		tag="v3.2.0"
-		curl -sLO https://github.com/ryanoasis/nerd-fonts/releases/download/$tag/$font.zip
-		sudo unzip -qo $font.zip -d /usr/local/share/fonts
-		rm $font.zip
+function install_virtualenv {
+	venvName="venv"
+	sudo pip -q install virtualenv
+	virtualenv -q $venvName
+}
+
+function installProgs {
+	progs=(alacritty btop go helm k9s kubectl nvim nvm virtualenv)
+	for prog in "${progs[@]}"; do
+		echo "checking $prog ..."
+		if ! command -v $prog &>/dev/null; then
+			echo "$prog not found! Installing ..."
+			install_$prog
+		fi
+		echo "$prog is ready!"
 	done
-	fc-cache -f
-	echo "nerd fonts is ready!"
 }
 
-function check_kubectl {
-	echo "checking kubectl ..."
-	which kubectl &>/dev/null
-	if [ $? != 0 ]; then
-		echo "installing kubectl ..."
-		tag="v1.28.7"
-		curl -sLO https://dl.k8s.io/release/$tag/bin/linux/amd64/kubectl
-		sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
-		rm kubectl
-	fi
-	echo "kubectl is ready!"
+function configProgs {
+	progs=(alacritty nerdfonts tmux tpm vim)
+	for prog in "${progs[@]}"; do
+		echo "configuring $prog ..."
+		config_$prog
+		echo "$prog has configured!"
+	done
 }
 
-function check_k9s {
-	echo "checking k9s ..."
-	which k9s &>/dev/null
-	if [ $? != 0 ]; then
-		echo "installing k9s ..."
-		tag="v0.32.4"
-		filename="k9s_Linux_amd64.tar.gz"
-		curl -sLO https://github.com/derailed/k9s/releases/download/$tag/$filename
-		sudo tar -zxf $filename -C /usr/local/bin
-		rm $filename
-	fi
-	echo "k9s is ready!"
-}
-
-function check_helm {
-	echo "checking helm ..."
-	which helm &>/dev/null
-	if [ $? != 0 ]; then
-		echo "installing helm ..."
-		tag="v3.14.3"
-		filename="helm-$tag-linux-amd64.tar.gz"
-		curl -sLO https://get.helm.sh/$filename
-		tar -zxf $filename
-		sudo mv ./linux-amd64/helm /usr/local/bin/
-		rm -rf ./linux-amd64
-		rm $filename
-	fi
-	echo "helm is ready!"
-}
-
-function update_bashrc {
-	echo "updating bashrc ..."
+function updateBashrc {
 	cp -f ./bashrc $HOME/.bashrc
 	echo "bashrc has been updated!"
 }
 
-install_pkgs
-check_btop
-check_alacritty
-update_alacritty_conf
-check_tpm
-update_tmux_conf
-update_vimrc
-check_neovim
-setup_lazyvim
-check_nvm
-check_go
-setup_python_venv
-setup_nerdfonts
-check_kubectl
-check_k9s
-check_helm
-update_bashrc
+essentials
+installProgs
+configProgs
+updateBashrc
 
 exit 0
