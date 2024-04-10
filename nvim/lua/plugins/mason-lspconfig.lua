033530212aff29eb8c53a5409cef4c95ef4752fa
willyhu.tw <willyhu.tw@gmail.com>
2024-04-10 08:19:05 +0800

    Add mason-lspconfig to install language servers automatically.

---
+++ nvim/lua/plugins/mason-lspconfig.lua
@@ -0,0 +1,19 @@
+return {
+	-- https://github.com/williamboman/mason-lspconfig.nvim
+	"williamboman/mason-lspconfig.nvim",
+	enabled = true,
+	dependencies = {
+		"williamboman/mason.nvim",
+		"neovim/nvim-lspconfig",
+	},
+	config = function()
+		require("mason-lspconfig").setup({
+			ensure_installed = {
+				"gopls",
+				"lua_ls",
+				"tsserver",
+			},
+			automatic_installation = true,
+		})
+	end,
+}
