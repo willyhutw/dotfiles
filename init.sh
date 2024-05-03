8577705093ec6b849269d690339a34648f6789f6
Willy Hu <willyhu.tw@gmail.com>
2024-05-03 23:34:05 +0800

    Fix virtualenv installation.

--- init.sh
+++ init.sh
@@ -206,7 +206,7 @@ function config_vim {
 }
 
 function install_virtualenv {
-	sudo pip -q --break-system-packages install virtualenv
+	sudo pip -q install virtualenv --break-system-packages
 	virtualenv -q ${HOME}/venv
 }
 
