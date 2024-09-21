e6130d413cc10e8a9b4b2899103b267e6b560ce5
Willy Hu <willyhu.tw@gmail.com>
2024-09-22 00:52:21 +0800

    Replace tsserver to ts_ls.

--- nvim/lua/plugins/mason-lspconfig.lua
+++ nvim/lua/plugins/mason-lspconfig.lua
@@ -1,21 +1,21 @@
 return {
-	-- https://github.com/williamboman/mason-lspconfig.nvim
-	"williamboman/mason-lspconfig.nvim",
-	enabled = true,
-	dependencies = {
-		"williamboman/mason.nvim",
-		"neovim/nvim-lspconfig",
-	},
-	config = function()
-		require("mason-lspconfig").setup({
-			ensure_installed = {
-				"bashls",
-				"gopls",
-				"lua_ls",
-				"tsserver",
-				"pyright",
-			},
-			automatic_installation = true,
-		})
-	end,
+  -- https://github.com/williamboman/mason-lspconfig.nvim
+  "williamboman/mason-lspconfig.nvim",
+  enabled = true,
+  dependencies = {
+    "williamboman/mason.nvim",
+    "neovim/nvim-lspconfig",
+  },
+  config = function()
+    require("mason-lspconfig").setup({
+      ensure_installed = {
+        "bashls",
+        "gopls",
+        "lua_ls",
+        "ts_ls",
+        "pyright",
+      },
+      automatic_installation = true,
+    })
+  end,
 }
