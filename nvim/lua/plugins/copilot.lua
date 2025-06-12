9c2c70f338f562b54a71732528a115bf18596159
Willy Hu <willyhu.tw@gmail.com>
2025-06-13 03:04:29 +0800

    Add github copilot related plugins to nvim.

---
+++ nvim/lua/plugins/copilot.lua
@@ -0,0 +1,40 @@
+return {
+	"zbirenbaum/copilot.lua",
+	cmd = "Copilot",
+	event = "BufReadPost",
+	config = function()
+		require("copilot").setup({
+			copilot_model = "gpt-4.1",
+			suggestion = {
+				enabled = true,
+				auto_trigger = true,
+				hide_during_completion = true,
+				debounce = 75,
+				trigger_on_accept = true,
+				keymap = {
+					accept = "<M-l>",
+					accept_word = false,
+					accept_line = false,
+					next = "<M-]>",
+					prev = "<M-[>",
+					dismiss = "<C-]>",
+				},
+			},
+			panel = {
+				enabled = false,
+			},
+			filetypes = {
+				gitcommit = false,
+				gitrebase = false,
+				help = false,
+				markdown = false,
+				yaml = false,
+				["."] = false,
+			},
+			workspace_folders = {},
+			root_dir = function()
+				return vim.fs.dirname(vim.fs.find(".git", { upward = true })[1])
+			end,
+		})
+	end,
+}
