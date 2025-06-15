6204fda9df2a8bc14830e7b135f1b1cb4425f58b
Willy Hu <willyhu.tw@gmail.com>
2025-06-15 16:59:03 +0800

    refactor(options): rewrite options for clarity and structure
    Refactored the Neovim options configuration to improve readability and
    organization. Replaced usage of `vim.opt` with explicit `vim.o` and
    `vim.g` assignments, grouped related settings, and added comments for
    clarity. Clipboard synchronization is now handled asynchronously with
    `vim.schedule`. This change aligns the options file with the structure
    from kickstart.nvim and removes redundant or unused settings.

--- nvim/lua/core/lazy.lua
+++ nvim/lua/core/lazy.lua
@@ -1,19 +1,17 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
 	vim.fn.system({
 		"git",
 		"clone",
 		"--filter=blob:none",
 		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable",
+		"--branch=main",
 		lazypath,
 	})
 end
-vim.opt.rtp:prepend(lazypath)
 
--- vim.g.mapleader = " "
-vim.g.mapleader = "\\"
-vim.g.maplocalleader = "\\"
+vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup("plugins", {
 	checker = {
@@ -22,6 +20,5 @@ require("lazy").setup("plugins", {
 	},
 	change_detection = {
 		enabled = false,
-		notify = true,
 	},
 })
--- nvim/lua/core/options.lua
+++ nvim/lua/core/options.lua
@@ -1,19 +1,39 @@
-local opt = vim.opt
+-- https://github.com/nvim-lua/kickstart.nvim/blob/master/init.lua
 
-opt.background = "dark"
-opt.clipboard = "unnamedplus"
-opt.cursorline = true
-opt.expandtab = true
-opt.ignorecase = true
-opt.mouse = "a"
-opt.number = true
-opt.relativenumber = false
-opt.shiftwidth = 2
-opt.signcolumn = "yes"
-opt.smartcase = true
-opt.softtabstop = 2
-opt.splitbelow = true
-opt.splitright = true
-opt.tabstop = 2
-opt.termguicolors = true
-opt.wrap = true
+-- golbal leader key
+vim.g.mapleader = "\\"
+vim.g.maplocalleader = "\\"
+
+-- display options
+vim.o.number = true
+vim.o.showmode = false
+vim.o.cursorline = true
+vim.o.signcolumn = "yes"
+vim.o.list = true
+vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
+
+-- editing options
+vim.o.expandtab = true
+vim.o.tabstop = 2
+vim.o.shiftwidth = 2
+vim.o.softtabstop = 2
+vim.o.breakindent = true
+vim.o.undofile = true
+
+-- search options
+vim.o.ignorecase = true
+vim.o.smartcase = true
+vim.o.inccommand = "nosplit"
+
+-- window options
+vim.o.splitright = true
+vim.o.splitbelow = true
+vim.o.confirm = true
+
+-- mouse support
+vim.o.mouse = "a"
+
+-- sync clipboard
+vim.schedule(function()
+	vim.o.clipboard = "unnamedplus"
+end)
