e8d9c050f9fc2916ea29b71b6e60cabadfabb228
Willy Hu <willyhu.tw@gmail.com>
2025-06-15 16:59:03 +0800

    refactor(options): rewrite options for clarity and structure
    Refactored the Neovim options configuration to improve readability and
    organization. Replaced usage of `vim.opt` with explicit `vim.o` and
    `vim.g` assignments, grouped related settings, and added comments for
    clarity. Clipboard synchronization is now handled asynchronously with
    `vim.schedule`. This change aligns the options file with the structure
    from kickstart.nvim and removes redundant or unused settings.

--- nvim/lua/core/keymaps.lua
+++ nvim/lua/core/keymaps.lua
@@ -1,4 +1,27 @@
-local keymap = vim.keymap
+vim.keymap.set("n", "<leader>l", "<cmd>Lazy<cr>")
+vim.keymap.set("n", "<leader>e", "<cmd>NvimTreeToggle<cr>")
 
-keymap.set("n", "<leader>l", "<cmd>Lazy<cr>")
-keymap.set("n", "<leader>e", "<cmd>NvimTreeToggle<cr>")
+-- TIP: Disable arrow keys in normal mode
+vim.keymap.set("n", "<left>", '<cmd>echo "Use h to move!!"<CR>')
+vim.keymap.set("n", "<right>", '<cmd>echo "Use l to move!!"<CR>')
+vim.keymap.set("n", "<up>", '<cmd>echo "Use k to move!!"<CR>')
+vim.keymap.set("n", "<down>", '<cmd>echo "Use j to move!!"<CR>')
+
+-- Keybinds to make split navigation easier.
+-- Use CTRL+<hjkl> to switch between windows
+vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
+vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
+vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
+vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
+
+-- Highlight when yanking (copying) text
+vim.api.nvim_create_autocmd("TextYankPost", {
+	desc = "Highlight when yanking (copying) text",
+	group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+	callback = function()
+		vim.hl.on_yank()
+	end,
+})
+
+-- Clear highlights on search when pressing <Esc> in normal mode
+vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
