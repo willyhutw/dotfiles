80c45fe3e4d03c120809145a7a37d0a40974bf7c
Willy Hu <willyhu.tw@gmail.com>
2025-06-15 13:17:18 +0800

    refactor(lspconfig): simplify lsp setup and lua_ls config
    Replaced per-server lspconfig setup with a direct call to
    vim.lsp.config for lua_ls, and removed redundant pyright and other
    server configurations. Updated lua_ls settings for improved workspace
    handling and diagnostics.

--- nvim/lua/plugins/nvim-lspconfig.lua
+++ nvim/lua/plugins/nvim-lspconfig.lua
@@ -8,40 +8,17 @@ return {
 		"hrsh7th/cmp-nvim-lsp",
 	},
 	config = function()
-		local on_attach = require("plugins.nvim-lspconfig").on_attach
-		-- local capabilities = require("plugins.lspconfig").capabilities
-		local capabilities = require("cmp_nvim_lsp").default_capabilities()
-		local lspconfig = require("lspconfig")
-
-		local servers = {
-			"lua_ls",
-			"bashls",
-			"gopls",
-			"ts_ls",
-			"pyright",
-		}
-
-		for _, lsp in ipairs(servers) do
-			lspconfig[lsp].setup({
-				on_attach = on_attach,
-				capabilities = capabilities,
-			})
-		end
-
-		-- Extra `global vim` setting
-		lspconfig["lua_ls"].setup({
+		vim.lsp.config("lua_ls", {
 			settings = {
 				Lua = {
 					runtime = {
-						-- Tell the language server which version of Lua you're using
 						version = "LuaJIT",
 					},
 					diagnostics = {
 						globals = { "vim" },
 					},
 					workspace = {
-						-- Make the server aware of Neovim runtime files
-						library = vim.api.nvim_get_runtime_file("", true),
+						checkThirdParty = false,
 					},
 					telemetry = {
 						enable = false,
@@ -49,9 +26,6 @@ return {
 				},
 			},
 		})
-
-		lspconfig["pyright"].setup({
-			filetypes = { "python" },
-		})
+		-- other lsp configurations can be added below
 	end,
 }
