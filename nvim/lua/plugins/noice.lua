86f072428d26861e1c56390c53f9bd9280455726
Willy Hu <willyhu.tw@gmail.com>
2025-06-16 12:31:58 +0800

    Add configuration for noice.nvim and nvim-notify plugins

---
+++ nvim/lua/plugins/noice.lua
@@ -0,0 +1,26 @@
+return {
+	-- https://github.com/folke/noice.nvim
+	"folke/noice.nvim",
+	enabled = true,
+	branch = "main",
+	event = "VeryLazy",
+	dependencies = {
+		"MunifTanjim/nui.nvim",
+		"rcarriga/nvim-notify",
+		"hrsh7th/nvim-cmp",
+	},
+	opts = {
+		lsp = {
+			override = {
+				["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+				["vim.lsp.util.stylize_markdown"] = true,
+				["cmp.entry.get_documentation"] = true,
+			},
+		},
+		presets = {
+			bottom_search = false,
+			command_palette = true,
+			long_message_to_split = false,
+		},
+	},
+}
