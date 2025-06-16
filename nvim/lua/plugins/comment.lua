775065dc0f024a46d2309fb43b60e05609fe6df3
Willy Hu <willyhu.tw@gmail.com>
2025-06-16 16:11:10 +0800

    feat(plugins): remove Comment.nvim plugin config
    Deleted the configuration file for the Comment.nvim plugin. This change
    removes the plugin from the setup, possibly to reduce dependencies or
    replace it with an alternative solution.

--- nvim/lua/plugins/comment.lua
+++
@@ -1,9 +0,0 @@
-return {
-	-- https://github.com/numToStr/Comment.nvim
-	"numToStr/Comment.nvim",
-	enabled = true,
-	branch = "master",
-	config = function()
-		require("Comment").setup({})
-	end,
-}
