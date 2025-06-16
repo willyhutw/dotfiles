fa15c6c6ff2e107b3c750595f29be8b50d9628b4
Willy Hu <willyhu.tw@gmail.com>
2025-06-17 01:38:49 +0800

    Replace copilot plugin with custom configuration for Lua

---
+++ nvim/lua/plugins/copilot.lua
@@ -0,0 +1,38 @@
+return {
+	-- https://github.com/zbirenbaum/copilot.lua
+	"zbirenbaum/copilot.lua",
+	enabled = true,
+	branch = "master",
+	cmd = "Copilot",
+	event = "BufReadPost",
+	config = function()
+		require("copilot").setup({
+			copilot_model = "gpt-4.1",
+			suggestion = {
+				enabled = true,
+				auto_trigger = true,
+				hide_during_completion = false,
+				debounce = 75,
+				trigger_on_accept = false,
+				keymap = {
+					accept = "<TAB>",
+					accept_word = false,
+					accept_line = false,
+					next = "<M-]>",
+					prev = "<M-[>",
+					dismiss = "<C-]>",
+				},
+			},
+			panel = {
+				enabled = false,
+			},
+			filetypes = {
+				["*"] = true,
+			},
+			workspace_folders = {},
+			root_dir = function()
+				return vim.fs.dirname(vim.fs.find(".git", { upward = true })[1])
+			end,
+		})
+	end,
+}
