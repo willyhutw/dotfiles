fcbd8f368866702f0573c6cc90b3da3da2b131a8
Willy Hu <willyhu.tw@gmail.com>
2025-06-16 13:01:37 +0800

    Remove unused configuration options from notify plugin

--- nvim/lua/plugins/nvim-notify.lua
+++ nvim/lua/plugins/nvim-notify.lua
@@ -4,24 +4,8 @@ return {
 	enabled = true,
 	branch = "master",
 	opts = {
-		background_colour = "NotifyBackground",
-		fps = 30,
-		icons = {
-			DEBUG = "",
-			ERROR = "",
-			INFO = "",
-			TRACE = "✎",
-			WARN = "",
-		},
 		level = 2,
-		minimum_width = 50,
 		render = "wrapped-compact",
 		stages = "fade",
-		time_formats = {
-			notification = "%T",
-			notification_history = "%FT%T",
-		},
-		timeout = 5000,
-		top_down = true,
 	},
 }
