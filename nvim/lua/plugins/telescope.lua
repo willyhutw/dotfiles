ee42cc6345cd3e3779f222f8e734534fb719f027
Willy Hu <willyhu.tw@gmail.com>
2025-06-16 12:31:16 +0800

    Add key mapping for searching with leader key

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -9,6 +9,9 @@ return {
 		"nvim-tree/nvim-web-devicons",
 		"folke/todo-comments.nvim",
 	},
+	keys = {
+		{ "<leader>s", "", desc = "+search", mode = { "n" } },
+	},
 	config = function()
 		require("telescope").setup({
 			extensions = {
@@ -24,11 +27,11 @@ return {
 		require("telescope").load_extension("fzf")
 
 		local builtin = require("telescope.builtin")
+		vim.keymap.set("n", "<leader><leader>", builtin.buffers, { desc = "[ ] Find existing buffers" })
 		vim.keymap.set("n", "<leader>sk", builtin.keymaps, { desc = "[S]earch [K]eymaps" })
 		vim.keymap.set("n", "<leader>sf", builtin.find_files, { desc = "[S]earch [F]iles" })
 		vim.keymap.set("n", "<leader>sg", builtin.live_grep, { desc = "[S]earch by [G]rep" })
 		vim.keymap.set("n", "<leader>s.", builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
-		vim.keymap.set("n", "<leader><leader>", builtin.buffers, { desc = "[ ] Find existing buffers" })
 		vim.keymap.set("n", "<leader>st", "<cmd>TodoTelescope<CR>", { desc = "[S]earch [T]odo comments" })
 	end,
 }
