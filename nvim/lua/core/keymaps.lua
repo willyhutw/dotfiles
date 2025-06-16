0983411a4aa55dbf1d4f751402c3f06d9165652a
Willy Hu <willyhu.tw@gmail.com>
2025-06-16 12:26:56 +0800

    Update git keymap shortcuts for git-related actions

--- nvim/lua/core/keymaps.lua
+++ nvim/lua/core/keymaps.lua
@@ -1,14 +1,5 @@
--- https://github.com/nvim-lua/kickstart.nvim/blob/master/init.lua
-
 vim.keymap.set("n", "<leader>l", "<cmd>Lazy<cr>")
 vim.keymap.set("n", "<leader>e", "<cmd>NvimTreeToggle<cr>")
-
--- Keybinds to make split navigation easier.
--- Use CTRL+<hjkl> to switch between windows
-vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
-vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
-vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
-vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
 
 -- Highlight when yanking (copying) text
 vim.api.nvim_create_autocmd("TextYankPost", {
@@ -25,13 +16,15 @@ vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
 -- Register keymap to the 'which-key' plugin
 local wk = require("which-key")
 wk.add({
-	{ "<leader>h", group = "+git" },
-	{ "<leader>hp", "<cmd>Gitsigns preview_hunk<CR>", desc = "git [p]review hunk" },
-	{ "<leader>hs", "<cmd>Gitsigns stage_hunk<CR>", desc = "git [s]tage hunk" },
-	{ "<leader>hu", "<cmd>Gitsigns undo_stage_hunk<CR>", desc = "git [u]ndo stage hunk" },
-	{ "<leader>hr", "<cmd>Gitsigns reset_hunk<CR>", desc = "git [r]eset hunk" },
-	{ "<leader>hS", "<cmd>Gitsigns stage_buffer<CR>", desc = "git [S]tage buffer" },
-	{ "<leader>hR", "<cmd>Gitsigns reset_buffer<CR>", desc = "git [R]eset buffer" },
-	{ "<leader>hb", "<cmd>Gitsigns blame_line<CR>", desc = "git [b]lame line" },
-	{ "<leader>hd", "<cmd>Gitsigns diffthis<CR>", desc = "git [d]iff" },
+	{ "<leader>h", group = "+git", mode = { "n", "v" } },
+	{ "<leader>hp", "<cmd>Gitsigns preview_hunk<CR>", desc = "[h]unk [p]review", mode = "n" },
+	{ "<leader>hs", "<cmd>Gitsigns stage_hunk<CR>", desc = "[h]unk [s]tage", mode = { "n", "v" } },
+	{ "<leader>hr", "<cmd>Gitsigns reset_hunk<CR>", desc = "[h]unk [r]eset", mode = { "n", "v" } },
+	{ "<leader>hu", "<cmd>Gitsigns undo_stage_hunk<CR>", desc = "[h]unk [u]ndo stage", mode = "n" },
+	{ "<leader>hS", "<cmd>Gitsigns stage_buffer<CR>", desc = "buffer [S]tage", mode = "n" },
+	{ "<leader>hR", "<cmd>Gitsigns reset_buffer<CR>", desc = "buffer [R]eset", mode = "n" },
+	{ "<leader>hb", "<cmd>Gitsigns blame_line<CR>", desc = "[b]lame line", mode = "n" },
+	{ "<leader>hd", "<cmd>Gitsigns diffthis<CR>", desc = "[d]iff", mode = "n" },
+	{ "]c", "<cmd>Gitsigns next_hunk<CR>", desc = "next git [c]hange", mode = "n" },
+	{ "[c]", "<cmd>Gitsigns prev_hunk<CR>", desc = "previous git [c]hange", mode = "n" },
 })
