452e83aada9528f900c7517d964423df13e7466d
Willy Hu <willyhu.tw@gmail.com>
2025-06-17 13:19:23 +0800

    Enable focused file and root update in nvim-tree

--- nvim/lua/plugins/nvim-tree.lua
+++ nvim/lua/plugins/nvim-tree.lua
@@ -7,6 +7,13 @@ return {
 	config = function()
 		vim.g.loaded_netrw = 1
 		vim.g.loaded_netrwPlugin = 1
-		require("nvim-tree").setup({})
+		require("nvim-tree").setup({
+			update_focused_file = {
+				enable = true,
+				update_root = {
+					enable = true,
+				},
+			},
+		})
 	end,
 }
